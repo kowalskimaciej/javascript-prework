04be24247c49196f69a8f49ff6ad73d532eeb7a2
Maciej Kowalski <maciej.kowalski1@proton.me>
2024-04-03 18:38:45 +0200

    Add games rules code js.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
-let playerMove = `papier`;
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+} else if(playerInput == '2'){
+  playerMove = 'papier';
+} else if(playerInput == '3'){
+  playerMove = 'nożyce';
+} else{
+      printMessage('Nieznany ruch');
+} 
+
+printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+      printMessage('Ty wygrywasz!');
+} else if( computerMove == 'kamień' && playerMove == 'kamień'){
+      printMessage('Remis!');
+} else if( computerMove == 'papier' && playerMove == 'papier'){
+      printMessage('Remis!');
+} else if( computerMove == 'papier' && playerMove == 'nożyce'){
+      printMessage('Ty wygrywasz!');
+} else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+      printMessage('Remis!');
+} else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+      printMessage('Ty wygrywasz!');
+} else{
+      printMessage('Ty przegrywasz!');
+}
